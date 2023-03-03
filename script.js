6d94aee47be0528f9e6b34f559fe9663afd70ece
Daniel Neyla <daniel.neyla@gmail.com>
2023-03-03 21:59:34 +0000

    Added the active behaviour of the ratings when they are clicked with js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const ratings = document.querySelectorAll(".rating-background");
+
+let activeRating = null;
+
+ratings.forEach((rating) => {
+  rating.addEventListener("click", () => {
+    if (activeRating) {
+      activeRating.classList.remove("clicked");
+      rating.classList.add("clicked");
+      activeRating = rating;
+      console.log(activeRating);
+    } else {
+      rating.classList.add("clicked");
+      activeRating = rating;
+    }
+  });
+});
+
+// const arr = [1, 3, 5];
+// const containsEvenNumber = arr.some((num) => {
+//   return num % 2 === 0;
+// });
+
+// console.log(containsEvenNumber);
