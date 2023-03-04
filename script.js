818b6412998ce2088d9df7cfdac352b82a8384f6
Daniel Neyla <daniel.neyla@gmail.com>
2023-03-04 00:25:10 +0000

    Done the javascript for the component change when the user submits a rating, now i only need to do minor changes

--- script.js
+++ script.js
@@ -2,25 +2,50 @@
 
 const ratings = document.querySelectorAll(".rating-background");
 
-let activeRating = null;
+let selectedRating = null;
 
 ratings.forEach((rating) => {
   rating.addEventListener("click", () => {
-    if (activeRating) {
-      activeRating.classList.remove("clicked");
+    if (selectedRating) {
+      selectedRating.classList.remove("clicked");
       rating.classList.add("clicked");
-      activeRating = rating;
-      console.log(activeRating);
+      selectedRating = rating;
+      console.log(selectedRating);
     } else {
       rating.classList.add("clicked");
-      activeRating = rating;
+      selectedRating = rating;
     }
   });
 });
 
-// const arr = [1, 3, 5];
-// const containsEvenNumber = arr.some((num) => {
-//   return num % 2 === 0;
-// });
+const submitBtn = document.querySelector(".btn-submit");
+const illustration = document.querySelector(".illustration-thank-you");
 
-// console.log(containsEvenNumber);
+const ratingComponent = document.querySelector(".rating-component");
+const thankYouComponent = document.querySelector(".thank-you-component");
+
+console.log(submitBtn, illustration);
+
+submitBtn.addEventListener("click", () => {
+  if (selectedRating) {
+    console.log(selectedRating);
+    const spanElement = selectedRating.querySelector("span");
+    const rating = spanElement.getAttribute("data-rating");
+
+    ratingComponent.style.display = "none";
+
+    const newParagraph = document.createElement("p");
+
+    newParagraph.classList.add("user-select");
+    newParagraph.textContent = `You selected ${rating} out of 5`;
+    illustration.insertAdjacentElement("afterend", newParagraph);
+
+    thankYouComponent.style.display = "block";
+    thankYouComponent.style.transition = "opacity 0.5s ease-in";
+    setTimeout(() => {
+      thankYouComponent.style.opacity = "1";
+    }, 1);
+  } else {
+    alert("You didnt select a rating");
+  }
+});
